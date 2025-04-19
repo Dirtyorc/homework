e5c32d2cd9963ab843f6e0e4eca7969e5dbcbc41
Dirtyorc <leshkapopovi484@gmail.com>
2025-04-19 22:35:59 +0300

    Add files via upload

---
+++ lesson-103/main.js
@@ -0,0 +1,41 @@
+const user = {
+  Aleksey: {
+    gender: 'male',
+    age: 33,
+    isAdmin: true,
+    // ==========2 задание=====
+    sayHello(name) {
+      console.log(`Hello ${name}`)
+    }
+  }
+}
+console.log(user.Aleksey)
+user.Aleksey.sayHello('Lexa')
+// =====3 задание=========
+const users = [
+  {
+    name: 'Aleksey',
+    age: 33,
+    isAdmin: true
+  },
+  {
+    name: 'Irina',
+    age: 28,
+    isAdmin: false
+  },
+  {
+    name: 'Dmitry',
+    age: 4,
+    isAdmin: false
+  },
+  {
+    name: 'Archi',
+    age: 8,
+    isAdmin: false
+  }
+
+]
+
+for(let i=0; i < users.length;i++)
+
+console.log(users[i].isAdmin)
